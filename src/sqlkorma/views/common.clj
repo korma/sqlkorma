89fde75eaf209b6e56212a4caea2c931f0f5b96f
Chris Granger <ibdknox@gmail.com>
2011-11-02 01:51:53 -0700

    add analytics
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/sqlkorma/views/common.clj
+++ src/sqlkorma/views/common.clj
@@ -4,6 +4,21 @@
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-css html5 link-to image]]))
 
+(defpartial analytics []
+  "<script type=\"text/javascript\">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26737307-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>"
+
 (defpartial footer []
   [:div#footer
     [:p "Copyright © 2011 " (link-to "http://chris-granger.com" "Chris Granger") ". All rights reserved."]])
@@ -23,7 +38,8 @@
               [:head
                [:title "sqlkorma"]
                (include-css "/css/reset.css")
-               (include-css "/css/default.css")]
+               (include-css "/css/default.css")
+               (analytics)]
               [:body
                [:div#wrapper
                 [:div#contentWrapper
