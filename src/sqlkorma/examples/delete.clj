44c528c516c6f28f3eae050669fc05e5de2fd00b
mynomoto <mynomoto@gmail.com>
2013-04-09 19:23:08 -0300

    Update src/sqlkorma/examples/delete.clj Changed the example to make it work.

--- src/sqlkorma/examples/delete.clj
+++ src/sqlkorma/examples/delete.clj
@@ -1,10 +1,10 @@
 (delete users
-  (where {:last_login [< last-year]}))
+  (where {:visits [< 3]})) 
 
 ;;You can also compose deletes:
 (def base (-> (delete* users)
-            (where {:last_login [< last-year]})))
+            (where {:visits [< 3]})))
 
 (-> base
   (where {:status [not= "active"]})
-  (exec))
+  (delete))
