f24f917901adea0e7992f51fcdc06eca4ba30b92
mynomoto <mynomoto@gmail.com>
2013-04-09 17:58:06 -0300

    Update src/sqlkorma/examples/db-intro.clj   Added helpers for h2 and msaccess in the list of helpers.   Changed the order of the example. Now first we define the connection map   with and without the helpers and only later define the connection.

--- src/sqlkorma/examples/db-intro.clj
+++ src/sqlkorma/examples/db-intro.clj
@@ -1,28 +1,42 @@
 (use 'korma.db)
+(require '[clojure.string :as str])
 
+(def db {:classname   "org.h2.Driver"
+         :subprotocol "h2"
+         :subname     "resources/db/korma.db"})
+;; this creates a connection map
+
+(def db (h2 {:db "resources/db/korma.db"}))
+;; this creates the same connection map as before, but
+;; using the helper (h2 ...).
+
+(def db (h2 {:db "resources/db/korma.db"
+             :user "sa"
+             :password ""
+             :naming {:keys str/lower-case
+                      ;; set map keys to lower
+                      :fields str/upper-case}}))
+                      ;; but field names are upper
+;; you can pass additional options
+
+;; Helpers for common databases:
+(def pg (postgres ..))
+(def ms (mssql ..))
+(def msac (msaccess ..))
+(def orc (oracle ..))
+(def mys (mysql ..))
+(def sqll (sqlite3 ..))
+(def h2-db (h2 ..))
+
+;; Pass the connection map to the defdb macro:
+(defdb korma-db db)
+
+;; Or without predefining a connection map:
 (defdb prod (postgres {:db "korma"
                        :user "korma"
                        :password "kormapass"
-                       ;;OPTIONAL KEYS
+                       ;; optional keys
                        :host "myhost"
                        :port "4567"
-                       :delimiters "" ;; remove delimiters
-                       :naming {:keys string/lower-case
-                                ;; set map keys to lower
-                                :fields string/upper-case}}))
-                                ;; but field names are upper
-
-;;Helpers for common databases:
-(defdb pg (postgres ..))
-(defdb ms (mssql ..))
-(defdb orc (oracle ..))
-(defdb mys (mysql ..))
-(defdb sqll (sqlite3 ..))
-
-;; Or you can simply pass in a jdbc database specification
-(defdb mydb {:classname   "org.h2.Driver"
-             :subprotocol "h2"
-             :subname "tcp://localhost:9092/Sample"
-             :user     "korma"
-             :password "kormapass"})
-
+                       :delimiters ""}))
+                       ;; remove delimiters 
