60708599535f9baf79fcd2fd66c29107445977f9
mynomoto <mynomoto@gmail.com>
2013-04-09 18:13:37 -0300

    Update src/sqlkorma/examples/entities.clj   The example works now. If you type it on a repl it order it won't   give any errors.   Added many-to-many example   Added prepare and transform function examples.   Tried to simplify the example moving some non default configurations   to other entities.

--- src/sqlkorma/examples/entities.clj
+++ src/sqlkorma/examples/entities.clj
@@ -1,30 +1,69 @@
 (use 'korma.core)
+(require '[clojure.string :as str])
+
+(declare users email address state account posts)
 
 (defentity users
-  (pk :my_pk) ;; By default "id"
-  (table :k_users) ;; By default the name of the symbol
-  (entity-fields :first :last) ;; Default fields for selects
-  (database prod) ;; if none is specified the last defdb
-                  ;; will be used
+  ;; Basic configuration
+  (pk :id) ;; by default "id". This line is unnecessary.
+           ;; it's used for relationships joins.
+  (table :users) ;; by default the name of the symbol.
+                 ;; The line above is also unecessary.
+  (database db) ;; if none is specified the last defdb
+                ;; will be used. Also unnecessary.
+  (entity-fields :first :last) ;; default fields for selects
 
-  ;; mutations
-  (prepare ..) ;; apply a function before storing in the db
-  (transform ..) ;; apply a function to all select results
+  ;; Mutations
+  (prepare (fn [{last :last :as v}]
+             (if last
+               (assoc v :last (str/upper-case last)) v)))
+  ;; apply a function before storing in the db
+  ;; in this case the function changes the "last" field
+  ;; to upper case.
+  (transform (fn [{first :first :as v}]
+               (if first
+                 (assoc v :first (str/capitalize first)) v)))
+  ;; apply a function to all select results
+  ;; in this case the function changes the "first" field
+  ;; to capitalized.
 
-  ;;Relationships
-  (has-one email)
-      ;; assumes users.id = email.users_id
-  (has-many address)
+  ;; Relationships
+  (has-one address)
       ;; assumes users.id = address.users_id
+  (has-many email)
+      ;; assumes users.id = email.users_id
       ;; but gets the results in a second query
       ;; for each element
   (belongs-to account)
       ;; assumes users.account_id = account.id
-  (has-one email {:fk :emailID}))
-      ;; you can optionally specify the foreign key
+  (many-to-many posts :users_posts))
+      ;; assumes a table users_posts with columns user_id
+      ;; and post_id
+      ;; like has-many, also gets the results in a second
+      ;; query for each element
 
-;;Subselects can be used as entities too!
+;; Subselects can be used as entities too!
 (defentity subselect-example
   (table (subselect users
             (where {:active true}))
          :activeUsers))
+
+(defentity email
+  (belongs-to users))
+
+(defentity address
+  (pk :my_pk) ;; sets the primary key to "my_pk"
+  (belongs-to users)
+  (belongs-to state {:fk :id_state}))
+      ;; you can optionally specify the foreign key
+      ;; assumes state.id = address.id_state
+
+(defentity state
+  (table :state_st) ;; sets the table to "state_st"
+  (has-many address))
+
+(defentity account
+  (has-one users))
+
+(defentity posts
+  (many-to-many users :users_posts))
